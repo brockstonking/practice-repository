28984dfdc53a108437daea3aa10a020faa76d75b
unknown <honorpurpose@gmail.com>
2019-03-05 15:55:49 -0700

    Initial Commit

---
+++ helloworld.js
@@ -0,0 +1,15 @@
+const companies = [
+    {name: "Company One", category: "finance", start: 1981, end: 2003},
+    {name: "Company Two", category: "metal", start: 1987, end: 2004},
+    {name: "Company Three", category: "sales", start: 1958, end: 2011},
+    {name: "Company Four", category: "industrial", start: 1984, end: 2009},
+    {name: "Company Five", category: "booking", start: 1956, end: 2005},
+    {name: "Company Six", category: "travel", start: 1997, end: 2008},
+]
+
+const ages = [1,2,3,4,5,6,7,8,9]
+
+for (let i = 0; i < companies.length; i++) {
+    console.log(companies[i]);
+}
+
